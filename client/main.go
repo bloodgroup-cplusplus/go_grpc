6ac2bbea923fe97659757d9cfebfbb99a741b567
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-10-08 13:00:38 +0530

    added grpc

--- client/main.go
+++ client/main.go
@@ -1,1 +1,16 @@
 package main
+
+
+import "net"
+
+const (
+	port = ":8080"
+)
+
+func main () {
+	lis,err := net.Listen("tcp",port)
+	if err !=nil{
+		log.Fatalf("Failed to start the server %v", err)
+	}
+
+	grpcServer := grpc.NewServer()
